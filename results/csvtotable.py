d23914aa65b58789a68f9fd6bdfb74e8b7b1ad70
Benedict B <bebo00001@stud.uni-saarland.de>
2023-08-19 17:59:35 +0200

    switched to tabularray tables added tables for parameter values

--- results/csvtotable.py
+++ results/csvtotable.py
@@ -1,6 +1,13 @@
 import csv
 import os
 
+import sys
+
+# weird hack because the python module system is a mess
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from benchmark import Benchmark
+
 
 def crawlDir(path):
     filtered_files = []
@@ -15,6 +22,8 @@ mappings = {"benchmark": "Benchmark"}
 
 def transform(val, label=False):
     val = mappings.get(val, val)
+    if val.isnumeric():
+        return "$" + val + "$"
     if label:
         return val.replace("_", r"\_\allowbreak ")
     return "\\" + val.replace("_", "")
@@ -26,20 +35,40 @@ def transformCSV(path):
         header = [transform(h, label=True) for h in next(reader)]
         rows = [
             [transform(val, label=(i == 0)) for i, val in enumerate(row)]
-            for row in list(reader)
+            for row in sorted(list(reader), key=lambda x: x[0])
         ]
-        # print(header, rows)
-    column_def = " A|" + "B" * (len(header) - 1) + " "
+    column_def = "@{} A|" + "B" * (len(header) - 1) + " @{}"
     header_def = " & ".join(header) + " \\\\\n"
     lastrow = rows.pop()  # to avoid trailing \\
     rows_def = "".join(
         [" & ".join(row) + " \\\\\n" for row in rows] + [" & ".join(lastrow)]
     )
-    return f"""\\begin{{tabular}}{{{column_def}}}
+    return f"""\\begin{{tblr}}{{colsep=5pt,colspec={{{column_def}}},width=\\textwidth}}
 {header_def}\\hline
 {rows_def}
-\end{{tabular}}%
+\end{{tblr}}%
 """
+
+
+def processBenchmarks(benchmarks, result_file):
+    rows = [
+        r"Benchmark & {Example Parameter Value 1} & {Example Parameter Value 2} & {Parameter Value to generate}\\",
+        "\hline",
+    ]
+    for bm in sorted(benchmarks, key=lambda x: x.name):
+        # incidentally, all benchmarks ONLY use "n" as a parameter name
+        row = (
+            [transform(bm.name, label=True)]
+            + [str(impl["params"]["n"]) for impl in bm.implementations]
+            + [str(bm.generate_params["n"])]
+        )
+        rows.append(" & ".join(row) + r"\\")
+    rows = "\n".join(rows)
+    table = f"""\\begin{{tblr}}{{colsep=5pt,colspec={{@{{}} A|XXX @{{}}}},width=\\textwidth}}
+{(rows)}
+\\end{{tblr}}"""
+    with open(result_file, "w") as f:
+        f.write(table)
 
 
 csv_files = crawlDir(".")
@@ -52,3 +81,18 @@ for file in csv_files:
             f.write(transformed)
     except StopIteration:
         print("Fail: " + file)
+
+processBenchmarks(
+    Benchmark.load_from_json("../benchmarks.json", "../parametric_solutions"),
+    "tex/benchmarks_human.tex",
+)
+
+processBenchmarks(
+    Benchmark.load_from_json("../benchmarks_strix.json", "../syntcomp/tlsf"),
+    "tex/benchmarks_strix.tex",
+)
+
+processBenchmarks(
+    Benchmark.load_from_json("../benchmarks_bosy.json", "../syntcomp/tlsf"),
+    "tex/benchmarks_bosy.tex",
+)
