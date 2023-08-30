d0149d805b7f8567cbb4947ea887241de986f42a
Benedict B <bebo00001@stud.uni-saarland.de>
2023-08-30 09:00:44 +0200

    docstring change; autoformat

--- syfco.py
+++ syfco.py
@@ -1,22 +1,47 @@
 import subprocess
+
 # Note: not all functions of syfco are supported
 
 syfco_path = "syfco"
+
+
 def _run_command(arguments, input):
-    out = subprocess.run(syfco_path + " " + arguments, input = input, shell=True, capture_output = True, text = True)
-    if(out.stderr != ""):
+    out = subprocess.run(
+        syfco_path + " " + arguments,
+        input=input,
+        shell=True,
+        capture_output=True,
+        text=True,
+    )
+    if out.stderr != "":
         raise Exception(out.stderr)
     return out.stdout
-    
-def _take_optional_args(overwrite_params : dict =  {}, transformations : list[str] = [], mode = "pretty", quote = "none"):
-    return " ".join(
-        ["-op " + key + "=" + str(value) for (key, value) in overwrite_params.items()]
-          + ["-" + tf for tf in transformations]
-        ) + " -m " + mode + " -q " + quote
-    
-def convert(input : str, format : str = "full", **kwargs):
+
+
+def _take_optional_args(
+    overwrite_params: dict = {},
+    transformations: list[str] = [],
+    mode="pretty",
+    quote="none",
+):
+    return (
+        " ".join(
+            [
+                "-op " + key + "=" + str(value)
+                for (key, value) in overwrite_params.items()
+            ]
+            + ["-" + tf for tf in transformations]
+        )
+        + " -m "
+        + mode
+        + " -q "
+        + quote
+    )
+
+
+def convert(input: str, format: str = "full", **kwargs):
     """
-    Takes a TLSF file as input and converts it to the output format specificied
+    Takes a TLSF specification as input and converts it to the output format specificied
     Optional Parameters:
       - overwrite_params: dict to overwrite parameters in the TLSF input
       - transformations: array that lists all of the transformations to apply, as described by syfco. Example could be ["s1"]
@@ -24,31 +49,38 @@ def convert(input : str, format : str = "full", **kwargs):
       - quote: "none" [default] do not quote identifiers; "double" quote identifiers using "
 
     """
-    args = f'-f {format} {_take_optional_args(**kwargs)} -in'
+    args = f"-f {format} {_take_optional_args(**kwargs)} -in"
     return _run_command(args, input).strip()
 
-def inputs(input : str, **kwargs):
-    args = f'-f ltl {_take_optional_args(**kwargs)} -in -ins'
+
+def inputs(input: str, **kwargs):
+    args = f"-f ltl {_take_optional_args(**kwargs)} -in -ins"
     return [s.strip() for s in _run_command(args, input).split(",")]
 
-def outputs(input : str, **kwargs) -> list[str]:
-    args = f'-f ltl {_take_optional_args(**kwargs)} -in -outs'
+
+def outputs(input: str, **kwargs) -> list[str]:
+    args = f"-f ltl {_take_optional_args(**kwargs)} -in -outs"
     return [s.strip() for s in _run_command(args, input).split(",")]
 
-def parameters(input : str):
-    args = f'-p -in '
+
+def parameters(input: str):
+    args = f"-p -in "
     return [s.strip() for s in _run_command(args, input).split(",")]
 
-def title(input : str) -> str:
-    args = f'-t -in'
+
+def title(input: str) -> str:
+    args = f"-t -in"
     return _run_command(args, input)[:-1]
-def description(input : str) -> str:
-    args = f'-d -in'
+
+
+def description(input: str) -> str:
+    args = f"-d -in"
     return _run_command(args, input)[:-1]
 
-def check(input : str) -> bool:
-    args = f'-c -in'
+
+def check(input: str) -> bool:
+    args = f"-c -in"
     try:
-      return "valid" in _run_command(args, input)    
+        return "valid" in _run_command(args, input)
     except Exception:
-      return False
+        return False
